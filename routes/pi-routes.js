18cbcb1f912e00d8b338e0d97651837fcca9e63f
sivaram <sivaram636@gmail.com>
2016-02-15 23:22:15 +0530

    basic switch

--- routes/pi-routes.js
+++ routes/pi-routes.js
@@ -1,12 +1,12 @@
-// var Gpio = require('onoff').Gpio,
-//   led = new Gpio(14, 'out');
+var Gpio = require('onoff').Gpio,
+  led = new Gpio(17, 'out');
  
 
 function toggleSwitch(req,res) {
-	// if(req.body.value){
-	// led.writeSync(1);	
-	// }
-	// led.writeSync(0);
+	if(req.body.value){
+	led.writeSync(1);	
+	}
+	led.writeSync(0);
 	res.status(200).send();	
 }
 
